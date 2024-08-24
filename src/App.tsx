49f19b20d65e5fa0968435dd9f55f818d47ac258
Serge Var <serge@pointnetwork.io>
2024-08-24 20:33:34 +0300

    Fix shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
+
+import { Button } from "@/components/ui/button"
 
 import {
   Card,
@@ -25,6 +26,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Button>Click me</Button>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react'
 
 import path from "path"
 
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './@/')
+      '@': path.resolve(__dirname, './src/')
     }
   }
 })
